2ac770e8017a93a85de2d0d233c21276ae752fda
nik frank <nikbfrank@gmail.com>
2014-09-08 15:45:28 +0300

    varstring lesson init

---
+++ main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// alright, let's learn about variables! what up
+// btw, don't worry about that first line. It just tells the browser about how to run the script
+// we put it everywhere, so if you don't care - don't - if you do, google it!
+
+// variables are all made the same way
+
+var name;
+
+// not like other languages where we have to decide what type it is upfront
+
+// we can also give them a value right away (on initialization)
+
+var age = 18;
+var country = 'Canada';
+
+// or make a bunch at once
+
+var province = 'BC', city = 'Vancouver';
