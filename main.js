0b6b82aaf9cdcdb669e455500163806a28284807
nik frank <nikbfrank@gmail.com>
2014-09-30 00:51:47 +0300

    actual lesson min

--- main.js
+++ main.js
@@ -1,20 +1,11 @@
 'use strict';
 
-// alright, let's learn about variables! what up
 // btw, don't worry about that first line. It just tells the browser about how to run the script
 // we put it everywhere, so if you don't care - don't - if you do, google it!
 
-// variables are all made the same way
+var name = 'Jerry';
 
-var name;
+if(name === 'Jerry Seinfeld'){
+    console.log('test #1 passed');
+}
 
-// not like other languages where we have to decide what type it is upfront
-
-// we can also give them a value right away (on initialization)
-
-var age = 18;
-var country = 'Canada';
-
-// or make a bunch at once
-
-var province = 'BC', city = 'Vancouver';
