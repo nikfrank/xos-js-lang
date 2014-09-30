13e539e8cdfa2b877ce48d3257f525494e7d39c7
nik frank <nikbfrank@gmail.com>
2014-10-01 02:09:39 +0300

    more tests

--- main.js
+++ main.js
@@ -3,9 +3,28 @@
 // btw, don't worry about that first line. It just tells the browser about how to run the script
 // we put it everywhere, so if you don't care - don't - if you do, google it!
 
-var age = 18;
+var age = 18, population, something, a,b,c,d;
 
-if(name === 26){
+if(age === 36){
     console.log('test #1 passed');
 }
 
+if(population > 1000000){
+    console.log('test #2 passed');
+}
+
+if((something > 3) && (something < 4)){
+    console.log('test #3 passed');
+}
+
+if(a*b === 48){
+    console.log('test #4 passed');
+}
+
+if((a%1 === 0) && (b%1 === 0) && ((b/a)%1 === 0.75)){
+    console.log('test #5 passed');
+}
+
+if((c+d === 32) && (c-d === 8)){
+    console.log('test #6 passed');
+}
