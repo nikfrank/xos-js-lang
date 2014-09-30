821843543491dfb9666e162f0b09bd8c30aede19
nik frank <nikbfrank@gmail.com>
2014-09-30 23:42:30 +0300

    unfinished arithmetic

--- main.js
+++ main.js
@@ -3,9 +3,9 @@
 // btw, don't worry about that first line. It just tells the browser about how to run the script
 // we put it everywhere, so if you don't care - don't - if you do, google it!
 
-var name = 'Jerry';
+var age = 18;
 
-if(name === 'Jerry Seinfeld'){
+if(name === 26){
     console.log('test #1 passed');
 }
 
