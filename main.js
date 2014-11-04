7aa5b97c19fa4a290e7956e75ad1afb2b51cc6c3
nik frank <nikbfrank@gmail.com>
2014-11-05 01:22:31 +0200

    done string code

--- main.js
+++ main.js
@@ -4,40 +4,56 @@
 // we put it everywhere, so if you don't care - don't - if you do, google it!
 
 
-// bugkill is init
-// first test is concat, escape, dereference, toUpperCase, toLowerCase, basic string replace
+// first test is concat, escape, dereference, toUpperCase, toLowerCase
 // second test is indexOf, substr, split, charCodeAt, casting numbers
 
 
-// this lesson needs something about escape chars
+var quote = 'these pretzels are makin' me thirsty;
 
-var name = 'Jerry', neighbour, actress, postman, greeting, child;
+var name = 'Jerry';
+
+// test #1
 
 if(name === 'Jerry Seinfeld'){
-    console.log('test #1 passed');
+    console.log('passed test #1 1/4');
 }
 
 if(neighbour + ' Kramer' === 'Cosmo Kramer'){
-    console.log('test #2 passed');
+    console.log('passed test #1 2/4');
 }
 
-if(greeting.substr(0,5) === 'Hello'){
-    console.log('test #3 passed');
-}
-
-if(greeting.indexOf(postman) !== -1){
-    console.log('test #4 passed');
+if(quote[0].toUpperCase() === quote[0]){
+    console.log('passed test #1 3/4');
 }
 
 if((actress[0] === 'J') && (actress[6] === 'L')){
-    console.log('test #5 passed');
+    console.log('passed test #1 4/4');
+}
+
+
+// test #2
+
+if(quote.indexOf('\'') !== -1){
+    console.log('passed test #2 1/6');
+}
+
+if(greeting.indexOf(postman) !== -1){
+    console.log('passed test #2 2/6');
+}
+
+if(greeting.substr(0,5) === 'Hello'){
+    console.log('passed test #2 3/6');
 }
 
 if(actress.split(' ')[1].split('-')[1] === 'Dreyfus'){
-    console.log('test #6 passed');
+    console.log('passed test #2 4/6');
+}
+
+if(postman.charCodeAt(2) === 119){
+    console.log('passed test #2 5/6');
 }
 
 if('' + 7 + ' Costanza' === child){
-    console.log('test #7 passed');
+    console.log('passed test #2 6/6');
 }
 
