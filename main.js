b42046a029597e49d6a101933889850628b237e3
nik frank <nikbfrank@gmail.com>
2014-11-04 22:33:59 +0200

    number work

--- main.js
+++ main.js
@@ -3,6 +3,12 @@
 // btw, don't worry about that first line. It just tells the browser about how to run the script
 // we put it everywhere, so if you don't care - don't - if you do, google it!
 
+
+// bugkill is init
+// test 1 is +-*/%,++,--, +=, -=, *=, /= comparisons
+// test 2 is Math.pow,floor,ceil,max,min,random,sqrt
+// test 3 is string conversions
+
 var age = 18, population, something, a,b,c,d;
 
 if(age !== 36){
