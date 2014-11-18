bb50c7bb8a98a5d1f8a918dfd328bc5d70e05d85
nik frank <nikbfrank@gmail.com>
2014-11-18 04:45:17 +0200

    obj started

--- main.js
+++ main.js
@@ -1,61 +1,43 @@
 'use strict';
 
-var foods = [];
+var students = [
+    {
+	firstName:'Nox',
+	lastName:'Freebird',
+	classes:[],
+	from:'England',
+	age:21,
+	languages:['en']
+    },
+    {firstName:'Svetlana', lastName:'Kirilenka', classes:[], from:'Russia', age:27, languages:['ru','iw']},
+    {firstName:'Moishe', lastName:'Mishu', classes:[], from:'Israel', age:25, languages:['iw','en']},
+    {firstName:'Mendel', lastName:'Mishu', classes:[], from:'Israel', age:24, languages:['iw','en']},
+    {firstName:'Menachem', lastName:'Mishu', classes:[], from:'Israel', age:26, languages:['iw','en']}
+];
 
-var tastyFoods = ['one oreo', 'package of oreos', 'package of doublestuf oreos',
-		  'dirty burger', 'mini dirty burger'];
+// note to the confused:: iw == hebrew (ע ~= i ו ~= w) it's part of ISO 639, so you'll see it sometimes. 'he' is also valid
+// google still uses it in translate.google.com urls (+ some DBs etc), so you may as well know what it is.
+// your complaints are legitimate. The guy who made it up in nineteen sixty whatever apologizes.
 
-var healthyFoods = ['potato', 'celery', 'carrot', 'banana', 'tofurkey'];
+var classes = [
+    {name:'Political Science', limit:4, students:[], languages:['en','ru']},
+    {name:'Math', limit:3, students:[], languages:['iw']},
+    {name:'Computer Science', limit:5, students:[], languages:['en']},
+    {name:'Physics', limit:10, students:[], languages:['iw', 'en']}
+];
 
-var allFoods = [tastyFoods, healthyFoods];
 
-var tastyFoodCalories = [120, 4320, 6500, 3000, 1548.236];
-var healthyFoodCalories = [46, 5, 4, 120, 300];
-
-var tastyFoodFat = [];
-var healthyFoodFat = [];
-
-var diet = [];
+classes[0].students.push(students[0]);
+students[0].classes.push(classes[0]);
 
 // test #1
 
-if(foods[0] === 'potato'){
-  console.log('passed test #1 1/4');
-}
+// student should alter a few fields, calculate whether courses are full
 
-if(foods.length === tastyFoods.length + healthyFoods.length){
-    console.log('passed test #1 2/4');
-}
+// test #2
 
-if(foods[6] === tastyFoods[1]){
-    console.log('passed test #1 3/4');
-}
+// student should write some functions which add / check / remove students, alter courses
 
-if(allFoods[0][0][0] === 'O'){
-    console.log('passed test #1 4/4');
-}
+// test #3
 
-
-
-// test #2 & 3
-
-// the user will fill in the functions under the given conditions
-
-// #2 (builtins) the user will write the functions without loops
-// #3 (looping) the user will write the functions without prototype methods
-
-function total(array){}
-function maxOrMin(array, maxOrMin){}
-function ascOrDesc(array, ascOrDesc){}
-function bounce(array){}
-function flatten(array){}
-function maxChunk(array, chunkSize){}
-function elimMinChunk(array, chunkSize){}
-
-// push & slice
-
-// max min, easy sorting, map
-// shift, unshift, splice, slice, filter
-// join, pop, push, reduce, concat
-
-// every, filter, forEach
+// student should do all of that using member functions on the objects
