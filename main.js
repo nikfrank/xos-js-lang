a38a5991fdf84e71ec9580cf82d638c3feb6e697
nik frank <nikbfrank@gmail.com>
2014-11-26 22:50:44 +0200

    title

--- main.js
+++ main.js
@@ -1,2 +1,36 @@
 'use strict';
 
+
+// just do a couple loops, then direct the student to the functions lesson
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function showResult(msg, passed){
+    console.dir(msg);
+    document.getElementById('results-container').innerHTML += '<p>'+msg+'</p>';
+}
