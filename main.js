10ecfa435481ff67df99bb5b33741c828f089384
nik frank <nikbfrank@gmail.com>
2014-12-04 01:30:49 +0200

    talks

--- main.js
+++ main.js
@@ -1,17 +1,13 @@
 'use strict';
 
-
-// just do a couple loops, then direct the student to the functions lesson
+// test #1
+// fill the arrays as indicated
 
 var squares = [];
 // squares should be an array of the square of its index of length > 100
 
 
 console.log(squares); // check it yourself! or test it officially
-
-if((squares.reduce(function(p,c,i){p&=(Math.sqrt(c)===i);},true))&&(squares.length>100)) 
-    showResult('Passed test #1 1/4');
-
 
 
 var fizzbuzz = [];
@@ -21,22 +17,22 @@ var fizzbuzz = [];
 
 
 
-
-
 console.log(fizzbuzz); // check it yourself! or test it officially
 
 
-if((fizzbuzz.reduce(function(p,c,i){p&=(i%3?true:(c.indexOf('fizz')>-1))&&(i%5?true:(c.indexOf('buzz')>-1));},true))&&(fizzbuzz.length===1000))
-    showResult('Passed test #1 2/4');
+
+// test #2
+// here we'll take an array of objects
+// we will turn one of the fields of each object into an index
+
+
+
+
+
 
 
 // once you've passed this test, go ahead and learn about functions
 // they will be necessary for all further tests (and a few you've already gone by)
-
-
-
-
-
 
 
 
