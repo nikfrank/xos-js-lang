0a01a0dda62d3b68d0ab93a3723f962f8d4cf1f0
nik frank <nikbfrank@gmail.com>
2014-12-03 21:34:31 +0200

    planned commented errors

--- main.js
+++ main.js
@@ -3,28 +3,8 @@
 
 
 
+// introduce the try,catch,finally system
 
-// test 1
-// define five functions, without implementing them - have the user implement them.
+// introduce throw and bubbling
 
-// test 2
-// have the user implement a function which takes a callback as a param
-
-// test 3
-// have the user implement a closure properly
-
-// test 4
-// have the user implement a timeout properly
-
-// test 5
-// have the user implement a timeout with a closure
-
-
-
-function total(array){}
-function maxOrMin(array, maxOrMin){}
-function ascOrDesc(array, ascOrDesc){}
-function bounce(array){}
-function flatten(array){}
-function maxChunk(array, chunkSize){}
-function elimMinChunk(array, chunkSize){}
+// in a later test, introduce the user to errorhandling in the context of promises
