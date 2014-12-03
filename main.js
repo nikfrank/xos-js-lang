87381078e0fd044597b5ba25b119dec0fc882f6d
nik frank <nikbfrank@gmail.com>
2014-12-03 21:26:44 +0200

    commented planned tests

--- main.js
+++ main.js
@@ -1,48 +1,25 @@
 'use strict';
 
-var foods = [];
-
-var tastyFoods = ['one oreo', 'package of oreos', 'package of doublestuf oreos',
-		  'dirty burger', 'mini dirty burger'];
-
-var healthyFoods = ['potato', 'celery', 'carrot', 'banana', 'tofurkey'];
-
-var allFoods = [tastyFoods, healthyFoods];
-
-var tastyFoodCalories = [120, 4320, 6500, 3000, 1548.236];
-var healthyFoodCalories = [46, 5, 4, 120, 300];
-
-var tastyFoodFat = [];
-var healthyFoodFat = [];
-
-var diet = [];
-
-// test #1
-
-if(foods[0] === 'potato'){
-  console.log('passed test #1 1/4');
-}
-
-if(foods.length === tastyFoods.length + healthyFoods.length){
-    console.log('passed test #1 2/4');
-}
-
-if(foods[6] === tastyFoods[1]){
-    console.log('passed test #1 3/4');
-}
-
-if(allFoods[0][0][0] === 'O'){
-    console.log('passed test #1 4/4');
-}
 
 
 
-// test #2 & 3
 
-// the user will fill in the functions under the given conditions
+// test 1
+// define five functions, without implementing them - have the user implement them.
 
-// #2 (builtins) the user will write the functions without loops
-// #3 (looping) the user will write the functions without prototype methods
+// test 2
+// have the user implement a function which takes a callback as a param
+
+// test 3
+// have the user implement a closure properly
+
+// test 4
+// have the user implement a timeout properly
+
+// test 5
+// have the user implement a timeout with a closure
+
+
 
 function total(array){}
 function maxOrMin(array, maxOrMin){}
@@ -51,11 +28,3 @@ function bounce(array){}
 function flatten(array){}
 function maxChunk(array, chunkSize){}
 function elimMinChunk(array, chunkSize){}
-
-// push & slice
-
-// max min, easy sorting, map
-// shift, unshift, splice, slice, filter
-// join, pop, push, reduce, concat
-
-// every, filter, forEach
