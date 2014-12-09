ae50f80f71241f3c6f88bfb010830c5f02e5456d
nik frank <nikbfrank@gmail.com>
2014-12-09 20:14:11 +0200

    restaurant for functions

--- main.js
+++ main.js
@@ -1,5 +1,103 @@
 'use strict';
 
+var customers = [];
+
+var currentCustomer = {
+    firstName:'Nik',
+    lastName:'Frank',
+    age:23,
+    regularOrder:['nana tea'],
+    tipAvg:0.18,
+    kosher:true,
+    vegan:false
+};
+
+
+var menu = [{
+    name:'coffee',
+    price:10,
+    calories:86,
+    type:'drink',
+    kashrutType:'pareve',
+    options:[{
+	name:'sugar',
+	price:0,
+	calories:100,
+	type:'condiment',
+	kashrutType:'pareve'
+    },{
+	name:'aspartame',
+	price:0,
+	calories:0,
+	type:'condiment',
+	kashrutType:'pareve'
+    },{
+	name:'creamer',
+	price:0,
+	calories:40,
+	type:'condiment',
+	kashrutType:'dairy'
+    }]
+},
+{
+    name:'toast',
+    price:30,
+    calories:400,
+    type:'food',
+    kashrutType:'pareve',
+    options:[{
+	name:'cheese',
+	price:8,
+	calories:115,
+	type:'food',
+	kashrutType:'dairy'
+    },{
+	name:'salmon',
+	price:16,
+	calories:130,
+	type:'food',
+	kashrutType:'pareve'
+    },{
+	name:'tofu',
+	price:20,
+	calories:99,
+	kashrutType:'pareve'
+    },{
+	name:'chicken',
+	price:20,
+	calories:135,
+	type:'food',
+	kashrutType:'meat'
+    }]
+}];
+
+
+// look at that readable data. niice.
+
+// first test is item lookups, indexing
+
+// second test is generating menus per diet
+
+// third test is checking diet reqs on orders and billing
+
+
+// the goal here is to fill in the following functions
+
+// check that the food is to the customer's kashrut & vegan standard
+// return the total price
+function order(customer, items, options){}
+
+function isKosher(item){}
+function isVegan(item){}
+
+function areKosher(items){}
+function areVegan(items){}
+
+function totalPrice(items){}
+
+
+// also, these functions should be linked to the dom in the next lesson
+
 
 
 
@@ -18,13 +116,3 @@
 
 // test 5
 // have the user implement a timeout with a closure
-
-
-
-function total(array){}
-function maxOrMin(array, maxOrMin){}
-function ascOrDesc(array, ascOrDesc){}
-function bounce(array){}
-function flatten(array){}
-function maxChunk(array, chunkSize){}
-function elimMinChunk(array, chunkSize){}
