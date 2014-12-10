d6f98193cc554d539f7dccfcbaae3a6c49868451
nik frank <nikbfrank@gmail.com>
2014-12-10 06:01:28 +0200

    tick tack

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var currentCustomer = {
     firstName:'Nik',
     lastName:'Frank',
     age:23,
-    regularOrder:['nana tea'],
+    regularOrder:['nana tea'], // make this an actual order array
     tipAvg:0.18,
     kosher:true,
     vegan:false
@@ -19,25 +19,11 @@ var menu = [{
     calories:86,
     type:'drink',
     kashrutType:'pareve',
-    options:[{
-	name:'sugar',
-	price:0,
-	calories:100,
-	type:'condiment',
-	kashrutType:'pareve'
-    },{
-	name:'aspartame',
-	price:0,
-	calories:0,
-	type:'condiment',
-	kashrutType:'pareve'
-    },{
-	name:'creamer',
-	price:0,
-	calories:40,
-	type:'condiment',
-	kashrutType:'dairy'
-    }]
+    options:[
+	{name:'sugar', price:0, calories:100, type:'condiment', kashrutType:'pareve'},
+	{name:'aspartame', price:0, calories:0, type:'condiment', kashrutType:'pareve'},
+	{name:'creamer', price:0, calories:40, type:'condiment', kashrutType:'dairy'}
+    ]
 },
 {
     name:'toast',
@@ -45,55 +31,52 @@ var menu = [{
     calories:400,
     type:'food',
     kashrutType:'pareve',
-    options:[{
-	name:'cheese',
-	price:8,
-	calories:115,
-	type:'food',
-	kashrutType:'dairy'
-    },{
-	name:'salmon',
-	price:16,
-	calories:130,
-	type:'food',
-	kashrutType:'pareve'
-    },{
-	name:'tofu',
-	price:20,
-	calories:99,
-	kashrutType:'pareve'
-    },{
-	name:'chicken',
-	price:20,
-	calories:135,
-	type:'food',
-	kashrutType:'meat'
-    }]
+    options:[
+	{name:'cheese', price:8, calories:115, type:'food', kashrutType:'dairy'},
+	{name:'salmon', price:16, calories:130, type:'food', kashrutType:'pareve'},
+	{name:'tofu', price:20, calories:99, kashrutType:'pareve'},
+	{name:'chicken', price:20, calories:135, type:'food', kashrutType:'meat'}
+    ]
 }];
 
-
-// look at that readable data. niice.
-
-// first test is item lookups, indexing
-
-// second test is generating menus per diet
-
-// third test is checking diet reqs on orders and billing
 
 
 // the goal here is to fill in the following functions
 
-// check that the food is to the customer's kashrut & vegan standard
-// return the total price
-function order(customer, items, options){}
+// items is an array of items with their options listed
 
-function isKosher(item){}
-function isVegan(item){}
+function order(customer, items){
 
-function areKosher(items){}
-function areVegan(items){}
+    // check that the food is to the customer's kashrut & vegan standard
+    if(customer.kosher) checkKosher(items);
+    if(customer.vegan) checkVegan(items);
 
-function totalPrice(items){}
+    // return the total price
+    return totalPrice(items);
+}
+
+function kashrutType(item){
+    // return either null, pareve, dairy, meat
+
+}
+
+function isVegan(item){
+    // return true or false
+}
+
+function checkKosher(items){
+    // loop through items into kashrutType
+    // check that their combination is kosher
+
+}
+
+function checkVegan(items){
+    // &= loop through items
+}
+
+function totalPrice(items){
+    // loop through, sum
+}
 
 
 // also, these functions should be linked to the dom in the next lesson
@@ -116,3 +99,7 @@ function totalPrice(items){}
 
 // test 5
 // have the user implement a timeout with a closure
+
+
+
+
